7e94f5a4dea7d91f13254ff4c1079f9cb75758ab
Lachlan Ridley <lachie.ridley@gmail.com>
2023-11-12 17:39:10 +1300

    Two triangles with two separate buffers

--- Main.cpp
+++ Main.cpp
@@ -89,32 +89,31 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	float vertices[] = {
+	float vertices1[] = {
 		-0.5f,  0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f,
 		-1.0f, -0.5f, 0.0f,
+		 0.0f, -0.5f, 0.0f
+	};
+
+	float vertices2[] = {
+		 0.5f,  0.5f, 0.0f,
 		 0.0f, -0.5f, 0.0f,
 		 1.0f, -0.5f, 0.0f
 	};
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 2, 3,   // first triangle
-		1, 4, 3    // second triangle
-	};
 
-	unsigned int VAO, VBO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &EBO);
-	glGenBuffers(1, &VBO);
+	unsigned int VAO[2], VBO[2];
+	glGenVertexArrays(2, &VAO[0]);
+	glGenBuffers(2, &VBO[0]);
 	
-	glBindVertexArray(VAO);
+	glBindVertexArray(VAO[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// link vertex attributes
+	glBindVertexArray(VAO[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -129,8 +128,12 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
+		glBindVertexArray(VAO[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(VAO[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
